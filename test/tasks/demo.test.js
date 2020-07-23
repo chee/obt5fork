80acee658d67a684cc5e4cbe66c4ae08d8609ed9
notlee <lee.moody@ft.com>
2020-07-23 10:09:55 +0100

    Update demo tests to check for success
    Currently the tests which intend to check demos build correctly
    for different configuration files/formats assert the first
    error thrown is not a given error. This commit updates these
    tests to confirm no errors are thrown at all by providing missing
    mustache templates and the demos we expect to be built are.
    
    All fixtures are reset after each test to help ensure one test
    does not interfere with another.

--- test/tasks/demo.test.js
+++ test/tasks/demo.test.js
@@ -15,12 +15,12 @@ const demoTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
 
 describe('Demo task', function() {
 
-	before(function() {
+	beforeEach(function() {
 		fs.copySync(path.resolve(obtPath, oTestPath), demoTestPath);
 		process.chdir(demoTestPath);
 	});
 
-	after(function() {
+	afterEach(function() {
 		process.chdir(obtPath);
 		fs.removeSync(demoTestPath);
 	});
@@ -38,95 +38,147 @@ describe('Demo task', function() {
 				});
 		});
 
-		it('should not error with a custom config file', function(done) {
-			fs.writeFileSync('bower.json', '{"name":"o-test"}', 'utf8');
-			fs.copySync('demos/src/config.json', 'demos/src/mysupercoolconfig.json');
-			const demoStream = demo(gulp, {
-				demoConfig: 'demos/src/mysupercoolconfig.json'
-			})
-			.on('error', function errorHandler(err) {
-				// It will throw a template not found error which is fixed in "should build html" test
-				proclaim.notEqual(err.message, 'Couldn\'t find demos config path, checked: demos/src/mysupercoolconfigs.json');
-				fs.unlink('demos/src/mysupercoolconfig.json');
-				demoStream.removeListener('error', errorHandler);
-				done();
-			});
+		it('should build demos defined in custom configuration files when specified', function (done) {
+			// Define template and custom config file
+			const testKey = 'custom-config';
+			const templatePath = `demos/src/${testKey}.mustache`;
+			const templateMarkup = `<div>${testKey}</div>`;
+			const configPath = `demos/src/${testKey}.json`;
+			const config = {
+				options: {
+					sass: 'demos/src/demo.scss',
+					js: 'demos/src/demo.js',
+					bodyClasses: testKey
+				},
+				demos: [
+					{
+						name: testKey,
+						description: 'A test demo.',
+						template: templatePath
+					}
+				]
+			};
+			// Write templates and config to the test directory
+			fs.writeFileSync(configPath, JSON.stringify(config), 'utf8');
+			fs.writeFileSync(templatePath, templateMarkup, 'utf8');
+			// Assert the template defined in custom config is built without error.
+			demo(gulp, { demoConfig: configPath })
+				.on('data', function (file) {
+					proclaim.include(file.path, `${testKey}.html`);
+					proclaim.include(file.contents.toString('utf8'), templateMarkup);
+					done();
+				}).on('error', function errorHandler(err) {
+					throw new Error('There was an error: ' + err.message);
+				});
 		});
 
-		it('should not fail using origami.json', function(done) {
+		it('should build demos defined in origami.json', function(done) {
+			const createdFiles = [];
 			const demoStream = demo(gulp, {
 				demoConfig: 'origami.json'
-			})
-			.on('error', function errorHandler(err) {
-				// It will throw a template not found error which is fixed in "should build html" test
-				proclaim.notEqual(err.message, 'Couldn\'t find demos config path, checked: origami.json');
-				demoStream.removeListener('error', errorHandler);
+			}).on('data', function (file) {
+				createdFiles.push({
+					path: file.path,
+					content: file.contents.toString('utf8'),
+				});
+			}).on('error', function(err) {
+				throw new Error('There was an error: ' + err.message);
+			}).on('end', function() {
+				const test = createdFiles.find(f => f.path.includes('test.html')) || {};
+				const pa11y = createdFiles.find(f => f.path.includes('pa11y.html')) || {};
+				proclaim.include(test.content, '<div>test</div>');
+				proclaim.include(pa11y.content, 'contrast');
 				done();
 			});
 		});
 
-		it('should not fail if there is a config.json file', function(done) {
-			const demoStream = demo(gulp)
-				.on('error', function errorHandler(err) {
-						// It will throw a template not found error which is fixed in "should build html" test
-						proclaim.notEqual(err.message, 'Couldn\'t find demos config path, checked: demos/src/config.json,demos/src/config.js,origami.json');
-						demoStream.removeListener('error', errorHandler);
-						done();
-					});
-		});
+		it('should build demos defined in origami.json using the old format', function(done) {
+			const demoConfig = JSON.parse(fs.readFileSync('origami.json', 'utf8'));
+			// In the old syntax demos are keys in an object, rather than within
+			// an array.
+			demoConfig.demos = {
+				test: {
+					description: 'First test',
+					template: 'demos/src/test.mustache'
+				}
+			};
+			// At some point `options` was used over `demosDefaults`.
+			delete demoConfig.demosDefaults;
+			demoConfig.options = {
+				sass: "demos/src/demo.scss",
+				js: "demos/src/demo.js",
+				bodyClasses: "old-config-test"
+			};
+			fs.writeFileSync('origami.json', JSON.stringify(demoConfig));
 
-		it('should not fail if there is a config.js file', function(done) {
-			const config = fs.readFileSync('demos/src/config.json');
-			fs.writeFileSync('demos/src/config.js', 'module.exports = ' + config, 'utf8');
-			const demoStream = demo(gulp)
-				.on('error', function errorHandler(err) {
-						// It will throw a template not found error which is fixed in "should build html" test
-						proclaim.notEqual(err.message, 'Couldn\'t find demos config path, checked: demos/src/config.json,demos/src/config.js,origami.json');
-						fs.unlink('demos/src/config.js');
-						demoStream.removeListener('error', errorHandler);
-						done();
-					});
-		});
-
-		it('should not fail if it\'s using the old config format', function(done) {
-			const demoStream = demo(gulp, {
-				demoConfig: 'demos/src/oldconfig.json'
-			})
-			.on('error', function errorHandler(err) {
-				proclaim.equal(err.message, 'Demo template not found: ' + path.resolve(process.cwd(), 'demos/src/test1.mustache'));
-				demoStream.removeListener('error', errorHandler);
+			demo(gulp, {
+				demoConfig: 'origami.json'
+			}).on('data', function (file) {
+				proclaim.include(file.path, 'test.html');
+				proclaim.include(file.contents.toString('utf8'), '<div>test</div>');
+				proclaim.include(file.contents.toString('utf8'), 'old-config-test');
 				done();
+			}).on('error', function errorHandler(err) {
+				throw new Error('There was an error: ' + err.message);
 			});
+		});
+
+		it('should build demos using the deprecated config.json file for demo configuration over origami.json if it exists', function (done) {
+			// Define template and demos/src/config.json
+			const testKey = 'demos-src-config-demo';
+			const templatePath = `demos/src/${testKey}.mustache`;
+			const templateMarkup = `<div>${testKey}</div>`;
+			const configPath = `demos/src/config.json`;
+			const config = {
+				options: {
+					sass: 'demos/src/demo.scss',
+					js: 'demos/src/demo.js',
+					bodyClasses: testKey
+				},
+				demos: [
+					{
+						name: testKey,
+						description: 'A test demo.',
+						template: templatePath
+					}
+				]
+			};
+			// Write templates and config to the test directory
+			fs.writeFileSync(configPath, JSON.stringify(config), 'utf8');
+			fs.writeFileSync(templatePath, templateMarkup, 'utf8');
+			// Assert the template defined in config.json is built without error.
+			demo(gulp)
+				.on('data', function(file) {
+					proclaim.include(file.path, `${testKey}.html`);
+					proclaim.include(file.contents.toString('utf8'), templateMarkup);
+					done();
+				}).on('error', function errorHandler(err) {
+					throw new Error('There was an error: ' + err.message);
+				});
 		});
 
 		it('should fail if there are demos with the same name', function(done) {
-			const demoConfig = JSON.parse(fs.readFileSync('demos/src/config.json', 'utf8'));
-			demoConfig.demos[1].name = 'test1';
-			fs.writeFileSync('demos/src/config2.json', JSON.stringify(demoConfig));
-			const demoStream = demo(gulp, {
-				demoConfig: 'demos/src/config2.json'
-			})
-			.on('error', function errorHandler(err) {
-				proclaim.equal(err.message, 'Demos with the same name were found. Give them unique names and try again.');
-				fs.unlink('demos/src/config2.json');
-				demoStream.removeListener('error', errorHandler);
-				done();
-			});
+			const demoConfig = JSON.parse(fs.readFileSync('origami.json', 'utf8'));
+			const existingDemoConfig = demoConfig.demos[1];
+			demoConfig.demos.push(Object.assign({}, existingDemoConfig));
+			fs.writeFileSync('origami.json', JSON.stringify(demoConfig));
+			const demoStream = demo(gulp)
+				.on('error', function errorHandler(err) {
+					proclaim.equal(err.message, 'Demos with the same name were found. Give them unique names and try again.');
+					done();
+				});
+			demoStream.resume();
 		});
 
 		it('should build demo html', function(done) {
-			fs.writeFileSync('demos/src/test1.mustache', '<div>test1</div>', 'utf8');
-			fs.writeFileSync('demos/src/test2.mustache', '<div>test2</div>', 'utf8');
 			const demoStream = demo(gulp)
 			.on('end', function() {
-					const test1 = fs.readFileSync('demos/test1.html', 'utf8');
-					const test2 = fs.readFileSync('demos/test2.html', 'utf8');
-					proclaim.include(test1, '<div>test1</div>');
-					proclaim.include(test2, '<div>test2</div>');
-					proclaim.match(test1, /\/v2\/polyfill\.min\.js\?features=.*promises/);
-					proclaim.match(test2, /\/v2\/polyfill\.min\.js\?features=.*promises/);
-					fs.unlink('demos/test1.html');
-					fs.unlink('demos/test2.html');
+					const test = fs.readFileSync('demos/test.html', 'utf8');
+					const pa11y = fs.readFileSync('demos/pa11y.html', 'utf8');
+					proclaim.include(test, '<div>test</div>');
+					proclaim.include(pa11y, 'No contrast');
+					proclaim.match(test, /\/v2\/polyfill\.min\.js\?features=.*promises/);
+					proclaim.match(pa11y, /\/v2\/polyfill\.min\.js\?features=.*promises/);
 					done();
 				});
 
@@ -134,16 +186,16 @@ describe('Demo task', function() {
 		});
 
 		it('should load local partials', function(done) {
-			fs.writeFileSync('demos/src/test1.mustache', '<div>test1</div>{{>partial1}}', 'utf8');
-			fs.writeFileSync('demos/src/test2.mustache', '<div>test1</div>{{>partials/partial2}}', 'utf8');
+			fs.writeFileSync('demos/src/test.mustache', `
+				<div>test</div>
+				{{>partial1}}
+				{{>partials/partial2}}
+			`, 'utf8');
 			const demoStream = demo(gulp)
 			.on('end', function() {
-				const test1 = fs.readFileSync('demos/test1.html', 'utf8');
-				const test2 = fs.readFileSync('demos/test2.html', 'utf8');
-				proclaim.include(test1, '<div>partial1</div>');
-				proclaim.include(test2, '<div>partial2</div>');
-				fs.unlink('demos/test1.html');
-				fs.unlink('demos/test2.html');
+				const test = fs.readFileSync('demos/test.html', 'utf8');
+				proclaim.include(test, '<div>partial1</div>');
+				proclaim.include(test, '<div>partial2</div>');
 				done();
 			});
 
