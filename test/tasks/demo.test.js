fc1a5581c7981adb358ee389859d20de3408ceaf
notlee <lee.moody@ft.com>
2020-07-23 17:50:03 +0100

    Do not change directory in demo tests
    origami build service does not do this

--- test/tasks/demo.test.js
+++ test/tasks/demo.test.js
@@ -13,41 +13,50 @@ const oTestPath = 'test/fixtures/o-test';
 const pathSuffix = '-demo';
 const demoTestPath = path.resolve(obtPath, oTestPath + pathSuffix);
 
+
+const projectRoot = path.join(__dirname, '../../');
+const testRoot = path.join(projectRoot, '/test');
+const fixturePath = path.join(testRoot, 'fixtures/o-test');
+
+
 describe('Demo task', function() {
+	let testPath;
+	let origamiManifestPath;
+	let demoDuplex;
 	let defaultOptions;
 
-	beforeEach(function() {
-		fs.copySync(path.resolve(obtPath, oTestPath), demoTestPath);
-		process.chdir(demoTestPath);
+	beforeEach(function () {
+		testPath = path.join(testRoot, `.test-run-demo-${Date.now()}`);
+		origamiManifestPath = path.join(testPath, 'origami.json');
+		fs.copySync(fixturePath, testPath);
 		defaultOptions = {
-			cwd: process.cwd()
+			cwd: testPath
 		};
 	});
 
-	afterEach(function() {
-		process.chdir(obtPath);
-		fs.removeSync(demoTestPath);
+	afterEach(function () {
+		// End the build duplex if it hasn't ended already.
+		demoDuplex.end();
+		fs.removeSync(testPath);
 	});
 
 	describe('Build demos', function() {
 		it('should fail if there is not a config file', function(done) {
-			fs.removeSync('origami.json');
-			const demoStream = demo(gulp, defaultOptions)
+			fs.removeSync(origamiManifestPath);
+			demoDuplex = demo(gulp, Object.assign({}, defaultOptions))
 				.on('error', function(err) {
 					proclaim.equal(err.message, 'Couldn\'t find demos config path, checked: origami.json,demos/src/config.json');
-					fs.unlink(path.resolve(obtPath, 'bower.json'));
-					process.chdir(demoTestPath);
 					done();
 				});
-			demoStream.resume();
+			demoDuplex.resume();
 		});
 
 		it('should build demos defined in custom configuration files when specified', function (done) {
 			// Define template and custom config file
 			const testKey = 'custom-config';
+			const configPath = `demos/src/${testKey}.json`;
 			const templatePath = `demos/src/${testKey}.mustache`;
 			const templateMarkup = `<div>${testKey}</div>`;
-			const configPath = `demos/src/${testKey}.json`;
 			const config = {
 				options: {
 					sass: 'demos/src/demo.scss',
@@ -63,8 +72,8 @@ describe('Demo task', function() {
 				]
 			};
 			// Write templates and config to the test directory
-			fs.writeFileSync(configPath, JSON.stringify(config), 'utf8');
-			fs.writeFileSync(templatePath, templateMarkup, 'utf8');
+			fs.writeFileSync(path.join(testPath, configPath), JSON.stringify(config), 'utf8');
+			fs.writeFileSync(path.join(testPath, templatePath), templateMarkup, 'utf8');
 			// Assert the template defined in custom config is built without error.
 			demo(gulp, Object.assign({}, defaultOptions, { demoConfig: configPath }))
 				.on('data', function (file) {
@@ -76,28 +85,27 @@ describe('Demo task', function() {
 				});
 		});
 
-		it('should build demos defined in origami.json', function(done) {
+		it('should build demos defined in origami.json by default', function(done) {
 			const createdFiles = [];
-			demo(gulp, Object.assign({}, defaultOptions, {
-				demoConfig: 'origami.json'
-			})).on('data', function (file) {
-				createdFiles.push({
-					path: file.path,
-					content: file.contents.toString('utf8'),
+			demo(gulp, Object.assign({}, defaultOptions))
+				.on('data', function (file) {
+					createdFiles.push({
+						path: file.path,
+						content: file.contents.toString('utf8'),
+					});
+				}).on('error', function(err) {
+					done(new Error('There was an error: ' + err.message));
+				}).on('end', function() {
+					const test = createdFiles.find(f => f.path.includes('test.html')) || {};
+					const pa11y = createdFiles.find(f => f.path.includes('pa11y.html')) || {};
+					proclaim.include(test.content, '<div>test</div>');
+					proclaim.include(pa11y.content, 'contrast');
+					done();
 				});
-			}).on('error', function(err) {
-				done(new Error('There was an error: ' + err.message));
-			}).on('end', function() {
-				const test = createdFiles.find(f => f.path.includes('test.html')) || {};
-				const pa11y = createdFiles.find(f => f.path.includes('pa11y.html')) || {};
-				proclaim.include(test.content, '<div>test</div>');
-				proclaim.include(pa11y.content, 'contrast');
-				done();
-			});
 		});
 
 		it('should build demos defined in origami.json using the old format', function(done) {
-			const demoConfig = JSON.parse(fs.readFileSync('origami.json', 'utf8'));
+			const demoConfig = JSON.parse(fs.readFileSync(origamiManifestPath, 'utf8'));
 			// In the old syntax demos are keys in an object, rather than within
 			// an array.
 			demoConfig.demos = {
@@ -113,11 +121,9 @@ describe('Demo task', function() {
 				js: "demos/src/demo.js",
 				bodyClasses: "old-config-test"
 			};
-			fs.writeFileSync('origami.json', JSON.stringify(demoConfig));
+			fs.writeFileSync(origamiManifestPath, JSON.stringify(demoConfig));
 
-			demo(gulp, Object.assign({}, defaultOptions, {
-				demoConfig: 'origami.json'
-			})).on('data', function (file) {
+			demo(gulp, Object.assign({}, defaultOptions)).on('data', function (file) {
 				proclaim.include(file.path, 'test.html');
 				proclaim.include(file.contents.toString('utf8'), '<div>test</div>');
 				proclaim.include(file.contents.toString('utf8'), 'old-config-test');
@@ -130,9 +136,9 @@ describe('Demo task', function() {
 		it('should build demos using the deprecated config.json file for demo configuration over origami.json if it exists', function (done) {
 			// Define template and demos/src/config.json
 			const testKey = 'demos-src-config-demo';
+			const configPath = path.join(testPath, `demos/src/config.json`);
 			const templatePath = `demos/src/${testKey}.mustache`;
 			const templateMarkup = `<div>${testKey}</div>`;
-			const configPath = `demos/src/config.json`;
 			const config = {
 				options: {
 					sass: 'demos/src/demo.scss',
@@ -149,9 +155,9 @@ describe('Demo task', function() {
 			};
 			// Write templates and config to the test directory
 			fs.writeFileSync(configPath, JSON.stringify(config), 'utf8');
-			fs.writeFileSync(templatePath, templateMarkup, 'utf8');
+			fs.writeFileSync(path.join(testPath, templatePath), templateMarkup, 'utf8');
 			// Assert the template defined in config.json is built without error.
-			demo(gulp, defaultOptions)
+			demo(gulp, Object.assign({}, defaultOptions))
 				.on('data', function(file) {
 					proclaim.include(file.path, `${testKey}.html`);
 					proclaim.include(file.contents.toString('utf8'), templateMarkup);
@@ -162,23 +168,23 @@ describe('Demo task', function() {
 		});
 
 		it('should fail if there are demos with the same name', function(done) {
-			const demoConfig = JSON.parse(fs.readFileSync('origami.json', 'utf8'));
+			const demoConfig = JSON.parse(fs.readFileSync(origamiManifestPath, 'utf8'));
 			const existingDemoConfig = demoConfig.demos[1];
 			demoConfig.demos.push(Object.assign({}, existingDemoConfig));
-			fs.writeFileSync('origami.json', JSON.stringify(demoConfig));
-			const demoStream = demo(gulp, defaultOptions)
+			fs.writeFileSync(origamiManifestPath, JSON.stringify(demoConfig));
+			demoDuplex = demo(gulp, Object.assign({}, defaultOptions))
 				.on('error', function errorHandler(err) {
 					proclaim.equal(err.message, 'Demos with the same name were found. Give them unique names and try again.');
 					done();
 				});
-			demoStream.resume();
+			demoDuplex.resume();
 		});
 
 		it('should build demo html', function(done) {
-			const demoStream = demo(gulp, defaultOptions)
+			demoDuplex = demo(gulp, Object.assign({}, defaultOptions))
 			.on('end', function() {
-					const test = fs.readFileSync('demos/test.html', 'utf8');
-					const pa11y = fs.readFileSync('demos/pa11y.html', 'utf8');
+					const test = fs.readFileSync(path.join(testPath, 'demos/test.html'), 'utf8');
+					const pa11y = fs.readFileSync(path.join(testPath, 'demos/pa11y.html'), 'utf8');
 					proclaim.include(test, '<div>test</div>');
 					proclaim.include(pa11y, 'No contrast');
 					proclaim.match(test, /\/v2\/polyfill\.min\.js\?features=.*promises/);
@@ -186,24 +192,24 @@ describe('Demo task', function() {
 					done();
 				});
 
-			demoStream.resume();
+			demoDuplex.resume();
 		});
 
 		it('should load local partials', function(done) {
-			fs.writeFileSync('demos/src/test.mustache', `
+			fs.writeFileSync(path.join(testPath, 'demos/src/test.mustache'), `
 				<div>test</div>
 				{{>partial1}}
 				{{>partials/partial2}}
 			`, 'utf8');
-			const demoStream = demo(gulp, defaultOptions)
+			demoDuplex = demo(gulp, Object.assign({}, defaultOptions))
 			.on('end', function() {
-				const test = fs.readFileSync('demos/test.html', 'utf8');
+				const test = fs.readFileSync(path.join(testPath, 'demos/test.html'), 'utf8');
 				proclaim.include(test, '<div>partial1</div>');
 				proclaim.include(test, '<div>partial2</div>');
 				done();
 			});
 
-			demoStream.resume();
+			demoDuplex.resume();
 		});
 	});
 });
